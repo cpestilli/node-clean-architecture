2672fb02ff35b4a6d5990fb2b78567a7810b93c9
Cleomar Pestilli <cleomar.pestilli@warren.com.br>
2022-07-13 22:48:26 -0300

    refactore: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,12 +3,19 @@ import { badRequest } from "../helpers/http-helper";
 import { MissingParamError } from "../errors/missing-param-error";
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpRequest {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError("name"));
-    }
+    // if (!httpRequest.body.name) {
+    //   return badRequest(new MissingParamError("name"));
+    // }
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError("email"));
+    // if (!httpRequest.body.email) {
+    //   return badRequest(new MissingParamError("email"));
+    // }
+
+    const requiredFields = ["name", "email"];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }

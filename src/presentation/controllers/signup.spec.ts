46300bd335e7d67f2e1cfbbbc607837da99aabd8
Cleomar Pestilli <cleomar.pestilli@warren.com.br>
2022-07-13 22:27:32 -0300

    refactore: add error type

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,10 +1,12 @@
 import { SignUpController } from "./signup";
+import { MissingParamError } from "../errors/missing-param-error";
 
 describe("SignUp Controller", () => {
   test("Should return 400 if no name is provided", () => {
     //system under test
     const sut = new SignUpController();
     const httpRequest = {
+      statusCode: 400,
       body: {
         email: "email",
         password: "password",
@@ -13,13 +15,14 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
   test("Should return 400 if no email is provided", () => {
     //system under test
     const sut = new SignUpController();
     const httpRequest = {
+      statusCode: 400,
       body: {
         name: "name",
         password: "password",
@@ -28,6 +31,6 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
 });
